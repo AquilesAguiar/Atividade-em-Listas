f78d2e1fd1892f3c351b58da41535c7f86f9548e
Aquiles Aguiar <aquilessilva@ucl.br>
2020-10-20 22:52:02 +0000

    nada

--- main.cs
+++ main.cs
@@ -7,7 +7,20 @@ class MainClass {
    string carroEco = '';
    double contadorEco = informacoes.lista_Consumo[0];
   
-   for(int x = 0;x> informacoes.lista_Carro;x++)
+   for(int x = 0;x > informacoes.lista_Carro;x++){
+     if(informacoes.lista_Consumo[x] > contadorEco ){
+       contadorEco = informacoes.lista_Consumo[x];
+       carroEco = informacoes.lista_Carro[x];
+     }
+   }
+
+   Console.WriteLine($"O modelo do carro mais econômico {carroEco}");
+
+   Console.WriteLine($"O consumo do carro mais econômico {contadorEco}");
+
+   for (int x = 0; i < informacoes.lista_Carro ;x++ ){
+     
+   }
   
 
   }
